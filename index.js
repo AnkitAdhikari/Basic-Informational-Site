92b2afb6f4712951dfb0d2aa07c5b1f0f4a16958
AnkitAdhikari <ankitadhikari31512@gmail.com>
2024-08-23 22:07:13 +0545

    update: now uses express insted of doing manually from node

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-const { createServer } = require('node:http');
-const fs = require("node:fs");
+const express = require('express');
+const path = require('path');
 
-const indexPage = fs.readFileSync('./index.html', 'utf-8', (err) => {
-    if (err)
-        console.error(err);
-});
+const app = express();
 
-const contactPage = fs.readFileSync('./contact-me.html', 'utf-8', err => {
-    if (err) {
-        console.error(err)
-    }
-})
-const aboutPage = fs.readFileSync('./about.html', 'utf-8', err => {
-    if (err) {
-        console.error(err)
-    }
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-const notfoundPage = fs.readFileSync('./404.html', 'utf-8', err => {
-    if (err) {
-        console.error(err)
-    }
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact-me.html'));
 })
 
-const server = createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        return res.end(indexPage);
-    } else if (req.url === '/contact-me') {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        return res.end(contactPage);
-    } else if (req.url === '/about') {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        return res.end(aboutPage);
-    } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end(notfoundPage);
-    }
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
 })
 
-server.listen(8080, '127.0.0.1');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '404.html'));
+})
+
+app.listen(PORT, () => {
+    console.log('server running...');
+})
